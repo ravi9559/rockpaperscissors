482de2e01c618c52794349ecd8d31f85dd9b62b0
Ravi Chaudhary <ravi.eng2012@gmail.com>
2022-04-06 19:06:47 +0530

    modified js file

--- external.js
+++ external.js
@@ -1,69 +1,77 @@
 
-const getUserChoice = (userInput) => {
- if (userInput.toLowerCase() === 'rock'){
-  return userInput;
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase()=="rock" && computerSelection[computerPlay]=="scissors" || playerSelection.toLowerCase()=="paper" && computerSelection[computerPlay]=="rock" || playerSelection.toLowerCase()=="scissors" && computerSelection[computerPlay]=="paper"){
+     playerScore++;
+    return win;
   }
- else if (userInput.toLowerCase() === 'paper'){
-    return userInput;
+  else if (playerSelection.toLowerCase()=="rock" && computerSelection[computerPlay]=="paper" || playerSelection.toLowerCase()=="scissors" && computerSelection[computerPlay]=="rock" || playerSelection.toLowerCase()=="paper" && computerSelection[computerPlay]=="scissors"){
+    computerScore++;
+    return lose;
   }
-  else if (userInput.toLowerCase() === 'scissors'){
-    return userInput;
-  }
-  else {console.log('Not Valid. Danger Will Robinson!')}
-  };
-
-const getComputerChoice = () => {
-  const randomNumber = (Math.floor(Math.random() * 3));
-  if (randomNumber === 0){
-    return 'Rock';
-  }
-  else if (randomNumber === 1){
-    return 'Paper';
-  }
-  else if (randomNumber === 2){
-  return 'Scissors';
-  }
-    }
-
-const determineWinner = (userChoice,computerChoice) => {
-  if (userChoice === computerChoice){
-    return 'The game is a tie!';
-  }
-  else if (userChoice === 'rock'){
-    if (computerChoice === 'paper'){
-      return 'The computer wins!';
-    }
-    else {
-      return 'You won!';
-    }
-  }
-  else if (userChoice === 'paper'){
-    if (computerChoice === 'scissors'){
-      return 'The computer wins!';
-    }
-    else {
-      return 'You won!';
-    }
-  }
-  else if (userChoice === 'scissors') {
-    if (computerChoice === 'rock') {
-      return 'The computer wins!';
-    }
-    else {
-      return 'You won!';
-    }
+    else if (playerSelection.toLowerCase() == computerSelection[computerPlay]){
+   tieScore++;
+    return tie;
+  } //if player input rock compare to rock, its a tie
+  else {
+    return "you must input rock, paper or scissors";
+  
+  } //else if player input paper compare to rock, you win
   }
   
-}
-
-
-const playGame = () => {
-  const userChoice = getUserChoice('scissors');
-  const computerChoice = getComputerChoice();
-console.log('You threw:' + userChoice);
-console.log('The computer threw:' + computerChoice);
-console.log(determineWinner(userChoice, computerChoice));
-};
-
-playGame();
-
+    let playerSelection = prompt("lets play rock papper scissors"); //define player, player input
+  //let player = playerSelection.toLowerCase(); //convert user input to lowercase *old
+  console.log ("you choose " +playerSelection.toLowerCase()); //show user input in lowercase
+  
+  let computerSelection = ["rock","paper","scissors"];//declare computerSelection
+  let computerPlay= Math.floor(Math.random() * computerSelection.length);//declare computerPlay
+  console.log("The computer choose " +computerPlay, computerSelection[computerPlay]);//define computer input
+    
+  //define scores
+  let playerScore=0;
+  let computerScore=0;
+  let tieScore=0;
+  
+  //define win, lose or tie
+  const win="You Win this round!\n -------";//define win
+  const lose="You Lose this round!\n -------";//define lose
+  const tie= "This round is a tie!\n -------";//define tie
+  
+  //define playround
+  console.log(playRound(playerSelection, computerSelection));
+  
+  
+  //loop
+  function game() {
+     
+   for (let i = 0; i <4; i++) { //what is going to be loop
+   playerSelection = prompt("lets play rock papper scissors"); //define player, player input
+  
+  console.log ("you choose " +playerSelection.toLowerCase()); //show user input in lowercase
+     
+   computerSelection = ["rock","paper","scissors"];//update computerSelection
+   computerPlay= Math.floor(Math.random() * computerSelection.length);//update computerPlay
+     
+   console.log("The computer choose " +computerPlay, computerSelection[computerPlay]);//define computer input  
+  
+  console.log(playRound(playerSelection, computerSelection)); 
+   }
+    if (playerScore>computerScore){
+       return "You Win this game.\n -------";
+     }
+       if (playerScore==computerScore){
+         return "This game is a tie.\n -------";
+       }
+      else{
+       return "You Lose this game.\n -------";
+     }
+  }
+  
+  console.log(game()); //play the loop
+  
+  console.log( "Your final score is " +playerScore +" wins " +computerScore +" loses and " +tieScore +" draws.\n -------");
+  
+  score=playerScore+computerScore+tieScore;
+  
+  console.log("You played this game " +score +" times.\n -------\nGame over.");
+  //count win lose or tie 
+  
